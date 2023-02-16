55f5c33e0729442c96943e22e0e3e40d6cf95c36
EC2 Default User <ec2-user@ip-172-31-38-95.ec2.internal>
2023-02-16 01:05:29 +0000

    Aula andor

--- meusite.py
+++ meusite.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Bem-vindo ao meu aplicativo Flask!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
 
     
